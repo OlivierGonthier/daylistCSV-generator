d95638cc62cfdb9e14afcae607d2647637d4a21d
Olivier Gonthier <o.gonthier@gmail.com>
2014-03-11 13:03:08 +0100

    Formatted date print

--- ListDaysOfMonth.hs
+++ ListDaysOfMonth.hs
@@ -1,14 +1,16 @@
 import Data.Time.Clock
 import Data.Time.Calendar
+import Data.Time.Format
+import System.Locale
 
 currentDate :: IO (Integer, Int, Int)
 currentDate = fmap (toGregorian . utctDay) getCurrentTime
-
+ 
 daysOfMonth :: Integer -> Int -> [Day]
 daysOfMonth year month = map (fromGregorian year month) [1..gregorianMonthLength year month]
-
+ 
 main = do
     (year, month, _) <- currentDate
     let days = daysOfMonth year month
-    putStrLn $ "Days: " ++ (show $ length days)
-    mapM_ print days
+    let daysString = map (formatTime defaultTimeLocale "%A %d %B %Y") days  
+    mapM_ print daysString
