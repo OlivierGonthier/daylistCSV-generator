bcd7c5aa65c575494909a7007c856a8fee0d2606
Olivier Gonthier <o.gonthier@gmail.com>
2014-03-11 13:15:00 +0100

    Extract day format in function

--- ListDaysOfMonth.hs
+++ ListDaysOfMonth.hs
@@ -8,9 +8,12 @@ currentDate = fmap (toGregorian . utctDay) getCurrentTime
  
 daysOfMonth :: Integer -> Int -> [Day]
 daysOfMonth year month = map (fromGregorian year month) [1..gregorianMonthLength year month]
- 
+
+formatDays :: [Day] -> [String]
+formatDays = map (formatTime defaultTimeLocale "%A %d %B %Y") 
+
 main = do
     (year, month, _) <- currentDate
     let days = daysOfMonth year month
-    let daysString = map (formatTime defaultTimeLocale "%A %d %B %Y") days  
-    mapM_ print daysString
+    let daysFormatted = formatDays days  
+    mapM_ print daysFormatted
