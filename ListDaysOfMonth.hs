ecdb73e0d61e0ffa71c2baffd92c231538195596
Olivier Gonthier <o.gonthier@gmail.com>
2014-03-18 00:41:41 +0100

    First attempt to get args from commandLine, separate and interpret them

--- ListDaysOfMonth.hs
+++ ListDaysOfMonth.hs
@@ -3,6 +3,7 @@ import Data.Time.Calendar
 import Data.Time.Format
 import Data.String.Utils
 import System.Locale
+import System.Environment
 import Text.CSV
 
 currentDate :: IO (Integer, Int, Int)
@@ -24,7 +25,23 @@ getRecord field
     | startswith "Sunday" field = [field]
     | otherwise = [field, "1"]
 
+groupArgs :: [String] -> [[String]]
+groupArgs = (map words).(filter ("" /=)).(split "-").unwords
+
+isolateArgWithValues :: [[String]] -> String -> [String] 
+isolateArgWithValues args option = head $ filter (\arg -> head arg == option) args
+
+getValues :: [[String]] -> String -> Int -> [String]
+getValues args option count = (take count).(drop 1) $ isolateArgWithValues args option
+
+extractValues :: [[String]] -> (String, String)
+extractValues args = (head $ getValues args "y" 1, head $ getValues args "m" 1)
+
 main = do
+    args <- getArgs
+    let groupedArgs = groupArgs args
+    let (argYear, argMonth) = extractValues groupedArgs
+    print (argYear, argMonth)
     (year, month, _) <- currentDate
     let days = daysOfMonth year month
     let daysFormatted = formatDays days  
