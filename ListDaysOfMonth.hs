579aa051a50cb62c92895f749501349ee048d9ce
Olivier Gonthier <o.gonthier@gmail.com>
2014-03-26 20:34:57 +0100

    Use getOpt to handle arguments

--- ListDaysOfMonth.hs
+++ ListDaysOfMonth.hs
@@ -5,6 +5,50 @@ import Data.String.Utils
 import System.Locale
 import System.Environment
 import Text.CSV
+import System.Console.GetOpt
+
+-- ====Args====
+data Opts = Opt
+    { optYear    :: Integer
+    , optMonth   :: Int
+    , optOutput  :: String
+    , optFR      :: Bool
+    , optHelp    :: Bool
+    }
+
+options :: [OptDescr (Opts -> IO Opts)]
+options =
+    [ Option ['y'] ["year"] 
+        (ReqArg 
+            (\arg opt -> return opt { optYear =  read arg :: Integer })
+            "YEAR") 
+        "Specify year, or current year by default"
+
+    , Option ['m'] ["month"] 
+        (ReqArg
+            (\arg opt -> return opt { optMonth = read arg :: Int })
+            "MONTH")
+        "Specify month, or current month by default"
+
+    , Option ['o'] ["output"] 
+        (ReqArg 
+            (\arg opt -> return opt { optOutput = arg })
+            "FILENAME") 
+        "Output result in file"
+
+    , Option []    ["fr"]
+        (NoArg
+            (\opt -> return opt { optFR = True })
+        )
+        "Use French words"
+
+    , Option []    ["help"] 
+        (NoArg 
+            (\opt -> return opt { optHelp = True })
+        )
+        "Print this help message"
+    ]
+
 
 currentDate :: IO (Integer, Int, Int)
 currentDate = fmap (toGregorian . utctDay) getCurrentTime
@@ -25,25 +69,27 @@ getRecord field
     | startswith "Sunday" field = [field]
     | otherwise = [field, "1"]
 
-groupArgs :: [String] -> [[String]]
-groupArgs = (map words).(filter ("" /=)).(split "-").unwords
+--groupArgs :: [String] -> [[String]]
+--groupArgs = (map words).(filter ("" /=)).(split "-").unwords
 
-isolateArgWithValues :: [[String]] -> String -> [String] 
-isolateArgWithValues args option = head $ filter (\arg -> head arg == option) args
+--isolateArgWithValues :: [[String]] -> String -> [String] 
+--isolateArgWithValues args option = head $ filter (\arg -> head arg == option) args
 
-getValues :: [[String]] -> String -> Int -> [String]
-getValues args option count = (take count).(drop 1) $ isolateArgWithValues args option
+--getValues :: [[String]] -> String -> Int -> [String]
+--getValues args option count = (take count).(drop 1) $ isolateArgWithValues args option
 
-extractValues :: [[String]] -> (String, String)
-extractValues args = (head $ getValues args "y" 1, head $ getValues args "m" 1)
+--extractValues :: [[String]] -> (Int, Int)
+--extractValues args = (read (head $ getValues args "y" 1)::Int, read(head $ getValues args "m" 1)::Int)
 
 main = do
     args <- getArgs
-    let groupedArgs = groupArgs args
-    let (argYear, argMonth) = extractValues groupedArgs
-    print (argYear, argMonth)
+    case getOpt Permute options args of
+        (args, _, []) -> do
+            putStrLn "It works!"
+        (_, _, errs) -> do
+            putStrLn "Arg Error"
     (year, month, _) <- currentDate
-    let days = daysOfMonth year month
+    let days = daysOfMonth (fromIntegral year) month
     let daysFormatted = formatDays days  
     let csv = getCSV daysFormatted
     putStrLn $ printCSV csv 
